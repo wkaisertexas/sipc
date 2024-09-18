8772a7b2b18bed074613753dd17f5a4f83a7a0f1
wkaisertexas <wkaisertexas@gmail.com>
2024-09-18 14:25:16 -0400

    added everything but for loop tests

--- docs/deliverables/deliverable1.md
+++ docs/deliverables/deliverable1.md
@@ -4,36 +4,36 @@ The goal of this document is to split tasks for writing unit tests and ensure we
 
 ## Boolean type with constants true and false, unary operator not, and non-short circuiting binary operators and and or.
 
-- [ ] true should be used for a literal
-- [ ] false should be used as a literal
-- [ ] you should be able to use and and or to combine expressions
-- [ ] Using xor should be an invalid operation
+- [x] true should be used for a literal
+- [x] false should be used as a literal
+- [x] you should be able to use and and or to combine expressions
+- [x] Using xor should be an invalid operation
 
 ## Array type with constructors [E1, ..., En] and [E1 of E2], a unary prefix length operator #, and a binary array element reference operator E1[E2]. Note that it is permissable to have an empty array in the first of these type constructors, e.g., []; the length of such an array is 0, i.e., #[] == 0.
 
-- [ ] Indexing into an array literal
-- [ ] Indexing into an array identifier
-- [ ] Getting the length of an array literal
-- [ ] Getting the length of an array
-- [ ] Making an array of another array
-- [ ] Constructing an array literal
+- [x] Indexing into an array literal
+- [x] Indexing into an array identifier
+- [x] Getting the length of an array literal
+- [x] Getting the length of an array
+- [x] Making an array of another array
+- [x] Constructing an array literal
 
 ## Arithmetic operators are extended with %, the modulo operator, and, the arithmetic negation operator.
 
-- [ ] Arithmetic negation is correctly applied and not mistaken for a minus sign
-- [ ] No negation takes place
-- [ ] The modulus operator has the same order of operations and multiplication and division
+- [x] Arithmetic negation is correctly applied and not mistaken for a minus sign
+- [x] No negation takes place
+- [x] The modulus operator has the same order of operations and multiplication and division
   
 ## Relational operators are extended with >=, <, and <=.
 
-- [ ] `>=` is a valid expression
-- [ ] `<=` is also a valid expression
-- [ ] `<` is a valid expression
+- [x] `>=` is a valid expression
+- [x] `<=` is also a valid expression
+- [x] `<` is a valid expression
 
 ## Ternary conditional expression operator E1 ? E2 : E3 which implements an if then else expression.
 
-- [ ] Ternaries parse properly
-- [ ] Nested ternaries parse properly
+- [x] Ternaries parse properly
+- [x] Nested ternaries parse properly
   
 ##  For loop using an iterator-style for (E1 : E2) S
 
---
+++ test/unit/frontend/SIPParserTest.cpp
@@ -0,0 +1,130 @@
+#include "ExceptionContainsWhat.h"
+#include "FrontEnd.h"
+#include "ParseError.h"
+#include "ParserHelper.h"
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("SIP Parser: conditionals", "[SIP Parser]") {
+  std::stringstream stream;
+  stream << R"(
+      short() {
+        var x, y, z;
+        if (x >= 0) {
+          while (y <= z) {
+            y = y + 1;
+          }
+        } else {
+          z = z + 1;
+        }
+        return z;
+      }
+    )";
+
+  REQUIRE(ParserHelper::is_parsable(stream));
+}
+
+TEST_CASE("SIP Parser: operators", "[SIP Parser]") {
+  std::stringstream stream;
+  stream << R"(
+      operators() {
+        var x;
+        x = x % 1;
+        x = x and 1;
+        x = x or 1;
+        x = x | 1;
+        x = x & 1;
+        x = x >= 1;
+        x = x <= 1;
+        x = x < 1;
+        x = x > 1;
+        return x;
+      }
+    )";
+
+  REQUIRE(ParserHelper::is_parsable(stream));
+}
+
+TEST_CASE("SIP Parser: arrays", "[SIP Parser]") {
+  std::stringstream stream;
+  stream << R"(
+      r() { var x, y; x = [1, 2, 3, 4, 5]; y = #x; y = x[0]; y = [1, 2, 3][1]; return x; }
+    )";
+
+  REQUIRE(ParserHelper::is_parsable(stream));
+}
+
+TEST_CASE("SIP Parser: ternary expr", "[SIP Parser]") {
+  std::stringstream stream;
+  stream << R"(
+      fn1() { var x, y; x = x ? x : y; return x + y; }
+      fn2() { var x, y; x = x ? x ? x : y : y; return x + y; }
+    )";
+
+  REQUIRE(ParserHelper::is_parsable(stream));
+}
+
+TEST_CASE("SIP Parser: bool literals", "[SIP Parser]") {
+  std::stringstream stream;
+  stream << R"(
+      main() { var x, y; x = true; y = false; return x + y; }
+    )";
+
+  REQUIRE(ParserHelper::is_parsable(stream));
+}
+
+TEST_CASE("SIP Parser: bool literals", "[SIP Parser]") {
+  std::stringstream stream;
+  stream << R"(
+      main() { var x, y; x = true; y = false; return x + y; }
+    )";
+
+  REQUIRE(ParserHelper::is_parsable(stream));
+}
+
+TEST_CASE("SIP Parser: boolean logic", "[SIP Parser]") {
+  std::stringstream stream;
+  stream << R"(
+      main() { var x, y; x = true; x = x and y or x | y; return x + y; }
+    )";
+
+  REQUIRE(ParserHelper::is_parsable(stream));
+}
+
+TEST_CASE("SIP Parser: plusplus and minus minus stmts", "[SIP Parser]") {
+  std::stringstream stream;
+  stream << R"(
+      main() { var x, y; x++; y--; return x + y; }
+    )";
+
+  REQUIRE(ParserHelper::is_parsable(stream));
+}
+
+/************ The following are expected to fail parsing ************/
+
+TEST_CASE("SIP Parser: unbalanced logical expression", "[SIP Parser]") {
+  std::stringstream stream;
+  stream << R"(
+      operators() { var x; x = y and or 1; return x; }
+    )";
+
+  REQUIRE_FALSE(ParserHelper::is_parsable(stream));
+}
+
+TEST_CASE("SIP Parser: unbalanced non-short-circuiting expression", "[SIP Parser]") {
+  std::stringstream stream;
+  stream << R"(
+      operators() { var x; x = y | & 1; return x; }
+    )";
+
+  REQUIRE_FALSE(ParserHelper::is_parsable(stream));
+}
+
+TEST_CASE("SIP Parser: xor (short circuiting expression is not supported)", "[SIP Parser]") {
+  std::stringstream stream;
+  stream << R"(
+      operators() { var x; x = y ^ 1; return x; }
+    )";
+
+  REQUIRE_FALSE(ParserHelper::is_parsable(stream));
+}
